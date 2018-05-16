4998624b7d1a141de8651557ff8f9c50a6a80df6
Rimcoin <38360606+rimcoin@users.noreply.github.com>
2018-05-16 14:33:41 -0400

    Add multi-core support.
    We have now added multi-core support by using the threading module. 🎉

--- mine.py
+++ mine.py
@@ -1,17 +1,27 @@
+from Crypto.Hash import SHA256
+from threading import Thread
 import os
 import sys
 import random
 import subprocess
 
-from Crypto.Hash import SHA256
-
 args = sys.argv
 diff = float(subprocess.check_output(("curl " + args[1] + "/get_mine -s").split(" ")))
-os.system("curl " + args[1] + "/new_mine -s")
+os.system("curl " + args[1] + "/new_mine -s > /dev/null")
 
-while True:
-    r = str(hex(random.randint(1, (2**32)))[2:])
-    Hash = SHA256.new()
-    Hash.update(r)
-    if int(Hash.hexdigest(), 16) < (diff):
-        os.system("curl " + sys.argv[1] + "/submit*" + r + "*" + sys.argv[2])
+def thread():
+    while True:
+        r = str(hex(random.randint(1, (2**32)))[2:])
+        Hash = SHA256.new()
+        Hash.update(r)
+        if int(Hash.hexdigest(), 16) < (diff):
+            os.system("curl " + sys.argv[1] + "/submit*" + r + "*" + sys.argv[2] + ">/dev/null")
+
+cores = float(raw_input("enter cores to mine on: "))
+n = 0
+
+while n<cores:
+    Thread(target=thread).start()
+    n+=1
+
+print("Mining... ")
